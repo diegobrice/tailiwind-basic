27f69362fc4b7bec64ac38f92f92c8f82aa0fda6
Diego Briceño <diegobrice@hotmail.com>
2020-09-06 15:31:11 -0500

    tailwind with npm

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      raleway: ["Raleway"],
+    },
+    extend: {
+      colors: {
+        primary: "#9cdbff",
+        secondary: {
+          100: "#9cabff",
+          200: "#9c00ff",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
